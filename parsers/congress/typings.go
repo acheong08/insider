6852e18dc5974498632b23fd9c37bb3479f462d3
Antonio <acheong@student.dalat.org>
2023-06-01 12:21:09 +0800

    congress typings

--- parsers/congress/typings.go
+++ parsers/congress/typings.go
@@ -2,7 +2,21 @@ package congress
 
 type Asset struct {
 	Name         string `json:"name"`
+	Ticker       string `json:"ticker"`
 	FilingStatus string `json:"filing_status"`
 	Description  string `json:"description"`
 	SubholdingOf string `json:"subholding_of"`
+}
+
+type Transaction struct {
+	Owner            string `json:"owner"`
+	Type             string `json:"type"`
+	Date             string `json:"date"`
+	NotificationDate string `json:"notification_date"`
+	AmountRange      Range  `json:"amount_range"`
+}
+
+type Range struct {
+	Min int `json:"min"`
+	Max int `json:"max"`
 }
